ae67e1642cdf4cb0bca06f969466a362658398d6
teluno <itsteluno@gmail.com>
2026-03-29 23:50:03 +0200

    move to solid and tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'node:url';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    }
+  },
+
+  integrations: [solidJs()]
+});
